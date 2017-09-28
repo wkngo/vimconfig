aad6278ddaba97a47b8e80d1058a3fc438fbb264
William Ngo <wingo7971@gmail.com>
2017-09-28 19:01:48 -0400

    Vundle Install

--- setup.sh
+++ setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-vundleDirectory="~/.vim/bundle/Vundle.vim"
-
-# checking if Vundle.vim exists
-if [ ! -d $vundleDirectory ]; then
-	git clone https://github.com/VundleVim/Vundle.vim.git $vundleDirectory
-else
-	echo "Directory already exists"
-fi
-
 cp -r ./vimrc ~/.vimrc
 echo "Copied vimrc to ~/.vimrc"
 
 cp -r ./vim ~/.vim
 echo "Copied vim to ~./vim"
 
+echo "Installing Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 echo "Finished setting up vim config"
+echo "Type ':PluginInstall' inside vim"
