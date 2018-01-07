34fb1d6199e8e8cc1d59c1af8474fb7989ec5513
William Ngo <wingo7971@gmail.com>
2018-01-07 15:56:42 -0500

    added i3 window manager config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp -r ./vimrc "${HOME}/.vimrc"
-echo "Copied vimrc to ${HOME}/.vimrc"
+cp -r ./vimrc "$HOME/.vimrc"
+echo "Copied vimrc to $HOME/.vimrc"
 
-cp -r ./vim ${HOME}
-echo "Copied vim to ${HOME}"
+cp -r ./vim $HOME
+echo "Copied vim to $HOME"
 
-mv ${HOME}/vim ${HOME}/.vim
+mv $HOME/vim $HOME/.vim
 echo "Renaming vim to .vim"
 
 echo "Installing Vundle"
@@ -14,3 +14,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Finished setting up vim config"
 echo "Type ':PluginInstall' inside vim"
+
+echo "Installing i3"
+mkdir -p "$HOME/.config" && cp -r ./i3 $_
